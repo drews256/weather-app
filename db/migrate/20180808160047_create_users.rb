2cd4d2776369a184b0234eba4f415a66e1ef4dbb
gkap98 <kaepernick.gavin6@gmail.com>
2018-08-08 11:08:21 -0700

    Added a Sign up and Login section to my weather main screen through the devise files

--- db/migrate/20180808160047_create_users.rb
+++ db/migrate/20180808160047_create_users.rb
@@ -1,10 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-
-      t.timestamps
-    end
+    add_column :users, :name, :string
+     add_column :users, :email, :string
   end
 end
